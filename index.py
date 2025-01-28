ecee74995a28395de4fa7f18031c4710e4ded1c2
Andreas Schmitt <62250169+Schmitt-A@users.noreply.github.com>
2025-01-28 21:55:47 +0100

    Update index.py

--- index.py
+++ index.py
@@ -5,3 +5,5 @@ st.write("Willkommen, *!!!* :sunglasses:")
 age = st.slider("How old are you?", 0, 130, 25)
 st.write("I'm ", age, "years old")
 
+st.secrets.database.password
+st.write(st.secrets.database.password)
