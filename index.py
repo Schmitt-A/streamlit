7132a4afcb4a6414fb16e0a0cb94e95f0d975d32
Andreas Schmitt <62250169+Schmitt-A@users.noreply.github.com>
2025-01-28 22:04:05 +0100

    test

--- index.py
+++ index.py
@@ -8,3 +8,8 @@ st.write("I'm ", age, "years 0ld")
 
 st.write(st.secrets.database.password)
 st.write(st.secrets.OpenAI_key)
+
+user = st.text_input("Benutzername", "")
+
+if user == st.secrets.name:
+    st.write("Angemeldet!")
