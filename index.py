8a48574067d08aeb32e51a1eaca92e5215cb3dd2
Andreas Schmitt <62250169+Schmitt-A@users.noreply.github.com>
2025-01-28 21:58:27 +0100

    Update index.py

--- index.py
+++ index.py
@@ -5,5 +5,6 @@ st.write("Willkommen, *!!!* :sunglasses:")
 age = st.slider("How old are you?", 0, 130, 25)
 st.write("I'm ", age, "years old")
 
-st.secrets.database.password
+
 st.write(st.secrets.database.password)
+st.write(st.secrets.OpenAI_key)
